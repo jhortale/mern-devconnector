bf373a4794a827e942c19915aee1d1fb38dc6ed5
Joao Hortale <jhortale@gmail.com>
2020-04-25 14:20:38 -0300

    ready to deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -11,13 +12,20 @@ app.use(express.json({
     extended: false
 }))
 
-app.get("/", (req, res) => res.send("API Running"));
-
 // Define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Server statit assets in production
+if (process.env.NODE_ENV === "production") {
+    // set static folder path
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
